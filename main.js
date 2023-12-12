b56d553f57e29306854d3d2cafa77085a6e481a3
matheus <matheus.resteves@gmail.com>
2023-12-12 17:21:01 -0300

    added: posição da camera inicial

--- main.js
+++ main.js
@@ -6,11 +6,10 @@ const scene = new THREE.Scene();
 scene.background = new THREE.Color(0x87ceeb);
 
 const camera = new THREE.PerspectiveCamera(40, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.set(18, 7, 12);
+camera.position.set(0, 3, 23.75);
 camera.lookAt(0, 1, 0);
 
 const renderer = new THREE.WebGLRenderer({ antialias: true });
-
 
 renderer.setSize( window.innerWidth, window.innerHeight );
 renderer.shadowMap.enabled = true;
